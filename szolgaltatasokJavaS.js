c1c2c724caf40c0f85f630ad5e37dba199ead47e
gabcsixx <103992637+gabcsixx@users.noreply.github.com>
2025-04-11 15:26:03 +0200

    a
    a

--- szolgaltatasokJavaS.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.toggle')
-const bemutato = document.querySelector('.bemutato')
-
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active')
-    bemutato.classList.toggle('active')
-})
